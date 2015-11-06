5485bfb4b15c3e67aa1f85eb951fb8d65d3ed1c2
Johnson Li <johnsonli1993@163.com>
2015-11-06 14:51:34 +0800

    nodes list & nodes add & nodes remove

---
+++ nodes/README.md
@@ -0,0 +1,6 @@
+Node Tools
+==
+
+All tools in this directory are used to operating Planet Lab nodes
+
+file `config.sh` is missing for that it stores some sensitive data
---
+++ nodes/addNodes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+source config.sh
+
+### data imported from config.sh
+cookie=$cookie
+slice_id=$slice_id
+
+data2=''
+while [[ $# > 0 ]]
+do
+    data2="$data2"$'------WebKitFormBoundaryjtuco1d5HBMH0Bzf\nContent-Disposition: form-data; name="node_ids[]"\n\n'"$1"$'\n'
+    shift
+done
+
+data1=$'------WebKitFormBoundaryjtuco1d5HBMH0Bzf\nContent-Disposition: form-data; name="slice_id"\n\n'"$slice_id"$'\n'
+data3=$'------WebKitFormBoundaryjtuco1d5HBMH0Bzf\nContent-Disposition: form-data; name="add-nodes-in-slice"\n\nAdd selected\n------WebKitFormBoundaryjtuco1d5HBMH0Bzf--\n'
+data="$data1$data2$data3"
+
+contentType='Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryjtuco1d5HBMH0Bzf'
+wget -qO- --header="$contentType" --header="$cookie" 'https://www.planet-lab.eu/db/common/actions.php' --post-data="$data" > /dev/null
+
+echo 'add action completed'
